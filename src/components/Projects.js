a6a5e00a196ee2af3f9a8cda24aed2e077c30b0b
Carlos C <ccastillomelgarejo@gmail.com>
2018-04-30 17:29:37 +0200

    home page web and style

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,9 +5,10 @@ export default class Projects extends Component {
   render() {
     return (
       <div className="Example">
-          <p className="App-intro">
-          Work in progress.
-        </p>
+          <div className="App-header-section-2">
+            <h1 className="headerText">Projects!</h1>
+            <h1 className="headerSubtext">Here you'll find some of my projects</h1>
+        </div>
       </div>
     );
   }
